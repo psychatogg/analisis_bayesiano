d48e6904de79ce819e6e2cc58de9f8657a5dcbea
ajulianaq <115423966+ajulianaq@users.noreply.github.com>
2023-04-04 13:31:12 +0200

    1.2

---
+++ tareas/3/1.2.R
@@ -0,0 +1,30 @@
+library(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+set.seed(1)
+
+datos <- list(n = length(y), x = x, y = y)
+codigo <- "
+  data {
+    int<lower=0> n;
+    int<lower=0> y[n];
+    int<lower=0> x[n];
+  }
+  
+  parameters {
+    real tau;
+    real omega;
+  }
+  model {
+  	real lambda;
+    tau ~ normal(0, 0.5);
+    omega ~ normal(0, 0.5);
+    for (i in 1:n) {
+    	lambda = exp(tau + omega*x[i]);
+      y[i] ~ poisson(lambda);
+    }
+  }
+"
+
+fit <- stan(model_code = codigo, data = datos, iter = 1000)
+print(fit)
