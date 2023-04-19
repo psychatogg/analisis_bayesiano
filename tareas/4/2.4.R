b3e9d63e1ef08c9c7541489184467f2afd3d07e5
ajulianaq <115423966+ajulianaq@users.noreply.github.com>
2023-04-19 13:42:44 +0200

    caambio a los ultimos 4000 puntos

--- tareas/4/2.4.R
+++ tareas/4/2.4.R
@@ -2,10 +2,10 @@ library(ggplot2)
 yPred_1 <- extract(fit1,"yPred")
 ## Empleo los últimos 10 valores de la última iteración
 yPred_1 <- yPred_1[[1]]
-yPred_1 <- yPred_1[2000,1:10]
+yPred_1 <- yPred_1[4000,1:10]
 yPred_2 <- extract(fit2,"yPred")
 yPred_2 <- yPred_2[[1]]
-yPred_2 <- yPred_2[2000,1:10]
+yPred_2 <- yPred_2[4000,1:10]
 
 data_1 <- as.data.frame( cbind(x,y,yPred_1))
 data_2 <- as.data.frame(cbind(x,y,yPred_2))
